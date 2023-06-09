f877fe084634779488f19620f185e3e6c9a7d32b
Muthu Raman <110217804+Ronaldo-00@users.noreply.github.com>
2023-06-09 09:56:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//open the main nav unorder list
+function opentoggle(){
+    document.getElementById("mainNav").style.left="0";
+}
+//close the main nav lists
+function closetoggle(){
+    document.getElementById("mainNav").style.left="-100%";
+}
+//open the nav
+function opennav(){
+    document.getElementById("navic").style.left="0";
+}
+
+function closenav(){
+    document.getElementById("navic").style.left="-100%";
+}
